83dce6574aa239b471ea4bd30e1c72d83dfa0bfe
iamjunhyeong <iamjunhyeong@github.com>
2024-06-24 03:11:36 +0900

    <fix>: user auth pattern

--- project/backend/domain/user/user_crud.py
+++ project/backend/domain/user/user_crud.py
@@ -9,14 +9,12 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_existing_user(db: Session, user_create: UserCreate):
     return db.query(User).filter(
-        (User.name == user_create.name) |
-        (User.email == user_create.email)
+        User.email == user_create.email
     ).first()
 
 
 def create_user(db: Session, user_create: UserCreate):
-
-    db_user = User(name=user_create.name,
+    db_user = User(username=user_create.name,
                    nickname=user_create.nickname,
                    email=user_create.email,
                    cellphone=user_create.cellphone,
@@ -29,10 +27,11 @@ def create_user(db: Session, user_create: UserCreate):
     db.add(db_user)
     db.commit()
 
+
 def update_user(db: Session, user_id: int, user_update: UserUpdate):
     user = db.query(User).filter(User.id == user_id).first()
     if user:
-        user.name = user_update.name or user.name
+        user.username = user_update.name or user.username
         user.email = user_update.email or user.email
         if user_update.password:
             user.password = pwd_context.hash(user_update.password)
@@ -40,8 +39,9 @@ def update_user(db: Session, user_id: int, user_update: UserUpdate):
         db.refresh(user)
     return user
 
+
 def update_external_id(db: Session, external_id: int, user_name: str):
-    user = db.query(User).filter(User.name == user_name).first()
+    user = db.query(User).filter(User.username == user_name).first()
     if user:
         user.external_id = external_id
         user.auth_type = "kakao"
@@ -50,9 +50,16 @@ def update_external_id(db: Session, external_id: int, user_name: str):
 
     return user
 
+
 def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
+
 
 def get_user_by_external_id(db: Session, external_id: int):
     return db.query(User).filter(User.external_id == external_id).first()
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_username(db: Session, _username: str):
+    return db.query(User).filter(User.username == _username).first()
