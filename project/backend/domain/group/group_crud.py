06fc10fd75bdb1c6a8a7b8095c21b0deee35efae
iamjunhyeong <iamjunhyeong@github.com>
2024-06-29 04:49:07 +0900

    <Feat>: group track

---
+++ project/backend/domain/group/group_crud.py
@@ -0,0 +1,49 @@
+from datetime import timedelta
+
+from sqlalchemy.orm import Session
+from models import Group, Track, Invitation, User
+
+from domain.group.group_schema import GroupCreate, InviteStatus
+
+
+
+
+def create_group(db: Session, _group: GroupCreate, track: Track, user_id: int):
+    db_group = Group(
+                    name=_group.name,
+                    track_id=track.id,
+                    user_id=user_id,
+                    start_day=_group.start_day,
+                    finish_day=_group.start_day + timedelta(days=track.duration)
+                    # 종료일 = 시작일 + (track.duration)일
+                )
+    db.add(db_group)
+    db.commit()
+
+
+def get_group_by_id(db, group_id):
+    return db.query(Group).filter(Group.id == group_id).first()
+
+
+def create_invitation(db: Session, user_id: int, group_id: int):
+    db_invitation = Invitation(
+        user_id=user_id,
+        group_id=group_id
+    )
+    db.add(db_invitation)
+    db.commit()
+
+
+def accept_invitation(db: Session, user_id: int, group_id: int):
+    invitation = db.query(Invitation).filter(Invitation.user_id == user_id,
+                                             Invitation.group_id == group_id,
+                                             Invitation.status == InviteStatus.PENDING).first()
+    invitation.status = InviteStatus.ACCEPTED
+    db.commit()
+
+    user = db.query(User).filter(User.id == user_id).one()
+    group = db.query(Group).filter(Group.id == group_id).one()
+    group.users.append(user)
+    db.commit()
+
+
