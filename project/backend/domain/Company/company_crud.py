4bf4dfb7ea369ecbcd6a73b0ecf34fbff30dec6e
iamjunhyeong <iamjunhyeong@github.com>
2024-06-29 04:04:21 +0900

    <Feat>: company crud

--- project/backend/domain/Company/company_crud.py
+++ project/backend/domain/Company/company_crud.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 
-from .company_schema import CompanyCreate
+from domain.Company.company_schema import CompanyCreate, CompanyUpdate
 from models import Company
+
 
 def company_create(_company_create: CompanyCreate, db: Session):
     db_company = Company(
@@ -11,4 +12,34 @@ def company_create(_company_create: CompanyCreate, db: Session):
         certificate=_company_create.certificate
         )
     db.add(db_company)
+    db.commit()
+
+
+def get_company_list(db: Session, skip: int = 0, limit: int = 10):
+    _company_list = db.query(Company).order_by(Company.name.desc())
+
+    total = _company_list.count()
+    company_list = _company_list.offset(skip).limit(limit).all()
+    return total, company_list
+
+
+def get_company_by_id(db: Session, company_id: int):
+    return db.query(Company).filter(Company.id == company_id).first()
+
+
+def company_update(db: Session, db_company: Company, _company_update: CompanyUpdate):
+    db_company.name = _company_update.name
+    db_company.owner = _company_update.owner
+    db_company.cellphone = _company_update.cellphone
+    db_company.certificate = _company_update.certificate
+    db.add(db_company)
+    db.commit()
+
+
+def get_company_by_name(db: Session, company_name: str):
+    return db.query(Company).filter(Company.name == company_name).first()
+
+
+def delete_company(db: Session, db_company: Company):
+    db.delete(db_company)
     db.commit()
