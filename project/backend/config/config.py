07e7c8c62038ca5391359086df2c3262b12ede52
Park-JunHyeong <115024272+iamjunhyeong@users.noreply.github.com>
2024-06-29 05:36:28 +0900

    Delete project/backend/config directory

--- project/backend/config/config.py
+++
@@ -1,7 +0,0 @@
-import pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env')
-    key: str
-
-settings = Settings(_env_file='.env')
