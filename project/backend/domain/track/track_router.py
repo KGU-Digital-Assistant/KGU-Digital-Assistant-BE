07ade1aa83ce60c1b6b78131b5163f9bd2d2fa4a
iamjunhyeong <iamjunhyeong@github.com>
2024-07-13 00:53:39 +0900

    <Feat:> track 기능 명세서에 맞게 수정

--- project/backend/domain/track/track_router.py
+++ project/backend/domain/track/track_router.py
@@ -14,23 +14,23 @@ router = APIRouter(
     prefix="/api/track",
 )
 
+
 @router.post("/create", status_code=status.HTTP_204_NO_CONTENT)
-def create_track(_track: TrackCreate,
-                 current_user: User = Depends(user_router.get_current_user),
+def create_track(current_user: User = Depends(user_router.get_current_user),
                  db: Session = Depends(get_db)):
-    track_crud.track_create(db, _track, current_user)
+    track = track_crud.track_create(db, current_user)
+    return {"track_id": track.id}
 
 
 @router.patch("/update", status_code=status.HTTP_204_NO_CONTENT)
-def update_track(track: TrackCreate,
+def update_track(_track_id: int,
+                 _track: TrackCreate,
                  current_user: User = Depends(user_router.get_current_user),
                  db: Session = Depends(get_db)):
-    if (current_user.id != track.user_id):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated",
-        )
-    track = track_crud.track_update(db, track, current_user)
+    track = track_crud.track_update(db, _track_id, current_user, _track)
+    if (track is None):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
     return track
 
 
@@ -59,12 +59,14 @@ def get_track_by_id(track_id: int, db: Session = Depends(get_db)):
 
 ###################################################
 
+
 @router.get("/get/{user_id}", response_model=track_schema.Track_schema)
 def get_Track_id(user_id: int, db: Session = Depends(get_db)):
     tracks = track_crud.get_Track_byuser_id(db, user_id=user_id)
     if tracks is None:
         raise HTTPException(status_code=404, detail="Track not found")
     return tracks
+
 
 @router.post("/post/{user_id})", response_model=track_schema.Track_create_schema)##회원일경우
 def post_Track(user_id: int, track: track_schema.Track_create_schema,db:Session=Depends(get_db)):
@@ -84,7 +86,7 @@ def post_Track(user_id: int, track: track_schema.Track_create_schema,db:Session=
     db.refresh(db_track)
 
     for routine in track.routines:
-        db_routine= TrackRoutine(
+        db_routine = TrackRoutine(
             track_id=db_track.id,
             title=routine.title,
             food=routine.food,
